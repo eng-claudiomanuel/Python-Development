aefebf816e3eba383bfb2eb595ef0717838cd9aa
eng-claudiomanuel <devclaudiomanuel@gmail.com>
2023-05-03 15:40:15 +0100

    added new line of code

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,5 @@
 #Created on 03 May 2023
-#Python Tutorial 
 #Hello World!
 print("Hello World of IoT and Development!")
-print("This is my First python file!")
+print("This is my First python file!\n")
+print("created by Claudio Manuel")
